1d6f8f192624f00e815e5f86cc81c3110cb01cb4
Venkatesh Naidu <venkynaidu1997@gmail.com>
2019-06-03 19:45:09 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React, {Component} from 'react';
+import './App.css';
+import Person from './Person/Person';
+import Radium from 'radium';
+
+class App extends Component {
+  state = {
+    Persons: [
+      {id: "id_1", name: "Venky", age: 21},
+      {id: "id_2", name: "Pudi", age: 22},
+      {id: "id_3", name: "Mansi", age: 25}
+    ]
+  };
+
+  changeHandler = (event, id) => {
+    let persons = [...this.state.Persons];
+    let index = persons.findIndex(person => {
+      return person.id === id
+    });
+    persons[index].name = event.target.value;
+    this.setState({
+      Persons: persons
+    });
+  };
+
+  deleteHandler = (index) => {
+    let persons = [...this.state.Persons];
+    persons.splice(index, 1);
+    this.setState({
+      Persons: persons
+    });
+  };
+
+  togglePersonsHandler = () => {
+    this.setState({
+      showPersons: !this.state.showPersons
+    });
+  };
+
+  render() {
+    const btn_style = {
+      backgroundColor: "green",
+      ':hover': {
+        backgroundColor: "lightgreen"
+      }
+    };
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {
+            this.state.Persons.map((person, index) => {
+              return <Person
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                click={this.deleteHandler.bind(this, index)}
+                change={(event) => this.changeHandler(event, person.id)}
+              />
+            })
+          }
+        </div>
+      );
+      btn_style.backgroundColor = "red";
+      btn_style[':hover'] = {
+        backgroundColor: "salmon"
+      };
+    }
+    return (
+      <div className="App">
+        <h1>Hi, This is Venky's first React App!!</h1>
+        <button className="btn" style={btn_style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        { persons }
+      </div>
+    );
+  }
+}
+
+export default Radium(App);
---
+++ src/Person/Person.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Person.css';
+import Radium from 'radium';
+
+const Person = (props) => {
+    return (
+        <div className="Person">
+            <p>I'm {props.name} and I'm - {props.age} years old.</p>
+            <input type="text" onChange={props.change} value={props.name} />
+            <p>{props.children}</p>
+            <button className="btn" onClick={props.click}>Delete</button>
+        </div>
+    );
+};
+
+export default Radium(Person);
